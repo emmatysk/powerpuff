9d9e7cdfdadb5f9e5dd9399853f39fb9b7d0d0e2
Niklas Persson <equadon@gmail.com>
2017-02-13 15:29:51 +0100

    Simple example structure.

---
+++ server.js
@@ -0,0 +1,48 @@
+// npm install express
+
+var path = require('path')
+var express = require('express')
+var expressVue = require('express-vue')
+var app = express();
+
+app.engine('vue', expressVue);
+app.set('view engine', 'vue');
+app.set('views', path.join(__dirname, '/views'));
+app.set('vue', {
+    componentsDir: path.join(__dirname, '/views/components'),
+    defaultLayout: 'layout'
+});
+
+var users = [];
+users.push({ name: 'tobi', age: 12 });
+users.push({ name: 'loki', age: 14  });
+users.push({ name: 'jane', age: 16  });
+
+app.get('/', function(req, res){
+  res.render('index', {
+    data: {
+        title: 'Express Vue',
+        message: 'Hello!',
+        users: users
+    },
+    vue: {
+        components: ['users', 'message']
+    }
+  });
+});
+
+app.get('/users/:userName', function(req, res){
+    var user = users.filter(function(item) {
+        return item.name === req.params.userName;
+    })[0];
+  res.render('user', {
+    data: {
+      title: 'Hello My Name is',
+      user: user
+    }
+
+  });
+});
+
+app.listen(3000);
+console.log('Express server listening on port 3000');
---
+++ views/components/users.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="">
+      <ul>
+          <li v-for="user in users">
+              <a v-bind:href="'/users/' + user.name">{{ user.name }}</a>
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['title', 'users']
+}
+</script>
+
+<style lang="css">
+</style>
