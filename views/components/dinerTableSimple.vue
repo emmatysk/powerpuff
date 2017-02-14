9e939769349f7643441e972d1cb562e81498bfc0
Niklas Persson <equadon@gmail.com>
2017-02-14 13:36:55 +0100

    Simple table overview page.

--- views/components/dinerTableSimple.vue
+++ views/components/dinerTableSimple.vue
@@ -1,7 +1,13 @@
 <template lang="html">
-  <li>
-    {{ table.id }}. {{ table.available }}/{{ table.capacity }} ({{ table.status }})
-  </li>
+  <tr v-bind:class="table.status">
+    <td>{{ table.id }}</td>
+    <td>{{ table.available }}</td>
+    <td>{{ table.capacity }}</td>
+    <td v-if="table.status == 'success'">Upptaget</td>
+    <td v-else-if="table.status == 'warning'">Väntar på mat</td>
+    <td v-else-if="table.status == 'danger'">PROBLEM!!!!</td>
+    <td v-else>Ledigt</td>
+  </tr>
 </template>
 
 <script>
--- views/overview.vue
+++ views/overview.vue
@@ -2,9 +2,30 @@
   <div>
     <h2>{{title}}</h2>
 
-    <ul>
-      <diner-table-simple :table="table" v-for="table in tables"></diner-table-simple>
-    </ul>
+    <div id="overview">
+      <ul>
+        <li>
+          Lediga bord: <strong>{{ availableTables }}</strong>
+        </li>
+        <li>
+          Lediga platser: <strong>{{ availableSeats }}</strong>
+        </li>
+      </ul>
+    </div>
+
+    <table class="table table-hover table-striped">
+      <thead>
+        <tr>
+          <th>Bord #</th>
+          <th>Ledigt</th>
+          <th>Platser</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <diner-table-simple :table="table" v-for="table in tables"></diner-table-simple>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -12,6 +33,30 @@
 export default {
   data: function() {
     return {}
+  },
+  computed: {
+    availableTables: function () {
+        var count = 0;
+
+        this.tables.forEach(function(table) {
+            if (table.status == 'available') {
+                count += 1;
+            }
+        });
+
+        return count;
+    },
+    availableSeats: function () {
+        var count = 0;
+
+        this.tables.forEach(function(table) {
+            if (table.status == 'available') {
+                count += table.capacity;
+            }
+        });
+
+        return count;
+    }
   }
 }
 </script>
