4c939491678f4a00dc744c3284cf264b04b1e696
Emma Tysk <emmatysk@student244-213.eduroam.uu.se>
2017-02-16 16:46:57 +0100

    Zup nigga

--- public/js/diner.js
+++ public/js/diner.js
@@ -13,28 +13,30 @@ function getOrderNumber() {
   return "#" + getRandomInt(1, 1000000);
 }
 
+
+
 new Vue({
   el: '#ordering',
   mixins: [sharedVueStuff], // include stuff that goes to both diner and kitchen
   data: {
+    selectedTable: ''
   },
   methods: {
-    placeOrder: function() {
-      // Here two ways of getting selected items are illustrated
-      // 1. The Vue way, notice the data model declarations above
-      var mainCourse = this.mainDish;
-      var extras = this.extras;
-      // 2. The old-school way: create an array with values of checked items
-      var theRest = [].filter.call(document.getElementsByName('item[]'), function(i) {
-        return i.checked;
-      }).map(function(i) {
-        return i.value;
-      });
-      // OK, it's not really neat to use two different ways of accomplishing the same thing
-      // but let's pretend it's for an educational purpose ... here comes another no-no:
-      var orderItems = mainCourse.concat(extras).concat(theRest);
-      // Finally we make use of socket.io's magic to send the stuff to the kitchen
-      socket.emit('order', {orderId: getOrderNumber(), orderItems: orderItems});
+    selectTable:function(number) {
+      if (number==this.selectedTable){
+          this.selectedTable = '';
+      }
+      else {
+          this.selectedTable = number;
+      }
+    },
+    addBorder() {
+
+    },
+    handler: function(number) {
+      this.selectTable(number);
+      this.addBorder();
     }
+
   }
 });
